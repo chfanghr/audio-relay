2c37eed4f8963ac488f16ed9688e5a1720e28a96
Hongrui Fang <chfanghr@gmail.com>
2026-09-23 00:56:37 +0800

    stupid

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ mod driver;
 use std::{thread, time::Duration};
 
 use anyhow::Result;
-use backon::{BlockingRetryableWithContext, ConstantBuilder};
 use log::info;
-
-use crate::driver::hat;
 
 const RPI4_I2C1_BUS_PATH: &str = "/dev/i2c-1";
 
@@ -21,24 +18,11 @@ fn main() -> Result<()> {
         for r in color_channel_range.clone() {
             for g in color_channel_range.clone() {
                 for b in color_channel_range.clone() {
-                    stupid
-                        .retry(ConstantBuilder::with_delay(
-                            ConstantBuilder::new(),
-                            Duration::from_millis(100),
-                        ))
-                        .context((&mut hat, r, g, b))
-                        .call()
-                        .1?;
+                    _ = hat.set_backlight_rgb(r, g, b);
+                    _ = hat.set_backlight_brightness((r + g + b) % 255);
                     thread::sleep(Duration::from_millis(100));
                 }
             }
         }
     }
-}
-
-type StupidInput<'a> = (&'a mut hat::Driver<linux_embedded_hal::I2cdev>, u8, u8, u8);
-
-fn stupid<'a>((hat, r, g, b): StupidInput<'a>) -> (StupidInput<'a>, Result<()>) {
-    let res = hat.set_backlight_rgb(r, g, b);
-    ((hat, r, g, b), res)
 }
