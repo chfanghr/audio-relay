cb1f288551fca0e523e9b05bdb577eff24bb9a2d
Hongrui Fang <chfanghr@gmail.com>
2026-09-22 22:06:10 +0800

    hat: sleep after creating i2c device to prevent errno 121

--- src/driver/hat.rs
+++ src/driver/hat.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{path::Path, thread, time::Duration};
 
 use super::pca9633;
 use anyhow::{Ok, Result};
@@ -26,6 +26,7 @@ impl<TDev: I2c> Driver<TDev> {
 impl Driver<linux_embedded_hal::I2cdev> {
     pub fn new_linux<P: AsRef<Path>>(p: P) -> Result<Self> {
         let rgb_dev = linux_embedded_hal::I2cdev::new(p)?;
+        thread::sleep(Duration::from_secs(1));
         Self::new(rgb_dev)
     }
 }
