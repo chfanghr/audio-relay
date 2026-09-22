d9f8cc0be37c119b84286bf67e91d85fda876a5d
Hongrui Fang <chfanghr@gmail.com>
2026-09-22 22:34:57 +0800

    hat: fix typo in comment

--- src/driver/pca9633.rs
+++ src/driver/pca9633.rs
@@ -70,8 +70,8 @@ impl<TDev: I2c> Driver<TDev> {
 
     pub fn init(&mut self) -> Result<()> {
         self.set_reg(Reg::Mode1, 0x00)?;
-        self.set_reg(Reg::LedOut, 0xFF)?; // set controlable by both PWM and GRPPWN registers
-        self.set_reg(Reg::Mode1, 0x20 | 0x05)?; // DMBLNK, OUTNE
+        self.set_reg(Reg::LedOut, 0xFF)?; // set controlable by both PWM and GRPPWM registers
+        self.set_reg(Reg::Mode1, 0x20)?; // DMBLNK
         self.set_rgb(0, 0, 0)?;
         Ok(())
     }
